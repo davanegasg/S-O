94c19002bcd122cfd7f6f58541612b4fb049a93b
davanegasg <68928488+davanegasg@users.noreply.github.com>
2023-03-09 06:53:29 -0500

    Proceso.java created

--- RoundRobin.java
+++ RoundRobin.java
@@ -37,25 +37,3 @@ public class RoundRobin {
         }
     }
 }
-
-class Proceso {
-    private String nombre;
-    private int duracion;
-
-    public Proceso(String nombre, int duracion) {
-        this.nombre = nombre;
-        this.duracion = duracion;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getDuracion() {
-        return duracion;
-    }
-
-    public void reducirDuracion(int tiempo) {
-        duracion -= tiempo;
-    }
-}
