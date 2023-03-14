89029f20e1f76496d9a4fa2237227719991a9cbf
davanegasg <68928488+davanegasg@users.noreply.github.com>
2023-03-14 07:51:28 -0500

    cambios finales

--- PriorityQ.java
+++ PriorityQ.java
@@ -6,11 +6,16 @@ public class PriorityQ{
         PriorityQueue<Proceso> cola = new PriorityQueue<>();
 
         // Agregar procesos a la cola
-        cola.add(new Proceso("P1", 20, 2));
-        cola.add(new Proceso("P2", 30, 1));
-        cola.add(new Proceso("P3", 10, 3));
-        cola.add(new Proceso("P4", 15, 2));
-
+        cola.add(new Proceso("P1", 2000, 7));
+        cola.add(new Proceso("P2", 3000, 2));
+        cola.add(new Proceso("P3", 1000, 5));
+        cola.add(new Proceso("P4", 1500, 4));
+        cola.add(new Proceso("P5", 2000, 10));
+        cola.add(new Proceso("P6", 3500, 3));
+        cola.add(new Proceso("P7", 1500, 1));
+        cola.add(new Proceso("P8", 5000, 8));
+        cola.add(new Proceso("P9", 4000, 7));
+        cola.add(new Proceso("P10", 2000, 1));
         // Ejecutar procesos en la cola
         while (!cola.isEmpty()) {
             Proceso proceso = cola.poll();
