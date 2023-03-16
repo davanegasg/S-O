d99f192602025c617ffb28d229869fffb35ff867
davanegasg <68928488+davanegasg@users.noreply.github.com>
2023-03-16 07:07:59 -0500

    Format tablet implemented

--- PriorityQ.java
+++ PriorityQ.java
@@ -6,33 +6,40 @@ public class PriorityQ{
         PriorityQueue<Proceso> cola = new PriorityQueue<>();
 
         // Agregar procesos a la cola
-        cola.add(new Proceso("P1", 100, 7));
-        cola.add(new Proceso("P2", 100, 2));
-        cola.add(new Proceso("P3", 100, 5));
-        cola.add(new Proceso("P4", 100, 4));
-        cola.add(new Proceso("P5", 100, 10));
-        cola.add(new Proceso("P6", 200, 3));
-        cola.add(new Proceso("P7", 200, 1));
-        cola.add(new Proceso("P8", 200, 8));
-        cola.add(new Proceso("P9", 200, 7));
-        cola.add(new Proceso("P10", 200, 1));
+        cola.add(new Proceso("P1", 1000, 7));
+        cola.add(new Proceso("P2", 1000, 2));
+        cola.add(new Proceso("P3", 1000, 5));
+        cola.add(new Proceso("P4", 1000, 4));
+        cola.add(new Proceso("P5", 1000, 10));
+        cola.add(new Proceso("P6", 2000, 3));
+        cola.add(new Proceso("P7", 2000, 1));
+        cola.add(new Proceso("P8", 2000, 8));
+        cola.add(new Proceso("P9", 2000, 7));
+        cola.add(new Proceso("P10", 2000, 1));
 
         int cont=0, tTotal =0;
         double tProm;
 
+        System.out.println("---------------------------------------------------------------------------");
+        System.out.printf("%10s %15s %15s", "PROCESO", "SEGUNDOS", "PRIORIDAD");
+        System.out.println();
+        System.out.println("---------------------------------------------------------------------------");
         // Ejecutar procesos en la cola
         while (!cola.isEmpty()) {
             Proceso proceso = cola.poll();
             tTotal += proceso.getDuracion();
             cont++;
-            System.out.println("Ejecutando proceso " + proceso.getNombre() + " (prioridad " + proceso.getPrioridad() + ") durante " + proceso.getDuracion() + " unidades de tiempo.");
+            System.out.format("%10s %15s %15s", proceso.getNombre(), Double.valueOf(proceso.getDuracion()/1000), proceso.getPrioridad());
+            System.out.println();
             try {
                 Thread.sleep(proceso.getDuracion());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        System.out.println("---------------------------------------------------------------------------");
         tProm = tTotal/cont;
-        System.out.println("Tiempo promedio de ejecución: " + (tProm/1000) + " Segundos");
+        System.out.println("TIEMPO PROMEDIO DE EJECICIÓN: " + (tProm/1000) + " SEGUNDOS");
+        System.out.println("---------------------------------------------------------------------------");
     }
 }
