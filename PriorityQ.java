92a87565a32fa1b69aa163a98a34eff063e8c179
davanegasg <68928488+davanegasg@users.noreply.github.com>
2023-03-16 06:36:16 -0500

    process time average added

--- PriorityQ.java
+++ PriorityQ.java
@@ -6,19 +6,25 @@ public class PriorityQ{
         PriorityQueue<Proceso> cola = new PriorityQueue<>();
 
         // Agregar procesos a la cola
-        cola.add(new Proceso("P1", 2000, 7));
-        cola.add(new Proceso("P2", 3000, 2));
-        cola.add(new Proceso("P3", 1000, 5));
-        cola.add(new Proceso("P4", 1500, 4));
-        cola.add(new Proceso("P5", 2000, 10));
-        cola.add(new Proceso("P6", 3500, 3));
-        cola.add(new Proceso("P7", 1500, 1));
-        cola.add(new Proceso("P8", 5000, 8));
-        cola.add(new Proceso("P9", 4000, 7));
-        cola.add(new Proceso("P10", 2000, 1));
+        cola.add(new Proceso("P1", 100, 7));
+        cola.add(new Proceso("P2", 100, 2));
+        cola.add(new Proceso("P3", 100, 5));
+        cola.add(new Proceso("P4", 100, 4));
+        cola.add(new Proceso("P5", 100, 10));
+        cola.add(new Proceso("P6", 200, 3));
+        cola.add(new Proceso("P7", 200, 1));
+        cola.add(new Proceso("P8", 200, 8));
+        cola.add(new Proceso("P9", 200, 7));
+        cola.add(new Proceso("P10", 200, 1));
+
+        int cont=0, tTotal =0;
+        double tProm;
+
         // Ejecutar procesos en la cola
         while (!cola.isEmpty()) {
             Proceso proceso = cola.poll();
+            tTotal += proceso.getDuracion();
+            cont++;
             System.out.println("Ejecutando proceso " + proceso.getNombre() + " (prioridad " + proceso.getPrioridad() + ") durante " + proceso.getDuracion() + " unidades de tiempo.");
             try {
                 Thread.sleep(proceso.getDuracion());
@@ -26,5 +32,7 @@ public class PriorityQ{
                 e.printStackTrace();
             }
         }
+        tProm = tTotal/cont;
+        System.out.println("Tiempo promedio de ejecución: " + (tProm/1000) + " Segundos");
     }
 }
